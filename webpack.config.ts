e3885c6916d15a98100423d7c0d0a4ec0c009484
prof-net <pavel.1c@mail.ru>
2023-03-23 14:32:33 +0300

    init

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from  'webpack';
+
+const config: webpack.Configuration = module.exports = {
+    mode: "development" as any,
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+        filename: '[name].[contenthash]bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
+
+export default config;
+
