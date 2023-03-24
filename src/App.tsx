58ccf1db5096446db5e83552f961abc8978d3ae1
prof-net <pavel.1c@mail.ru>
2023-03-24 12:45:29 +0300

    code splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPages/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPages/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            sefsefsefsef sefse
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О Компании</Link>
+            <Suspense fallback={<div>Загрузка</div>} >
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+
         </div>
     );
 };
---
+++ src/pages/AboutPages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            ABOUT PAGES
+        </div>
+    );
+};
+
+export default AboutPage;
